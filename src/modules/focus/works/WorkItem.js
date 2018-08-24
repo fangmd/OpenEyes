769f90b0ca84f78154d0674823e5fcab6220d794
fangmingdong <fangmingdong@lianhejinxin.com>
2018-08-24 13:57:54 +0800

    video

--- src/modules/focus/works/WorkItem.js
+++ src/modules/focus/works/WorkItem.js
@@ -6,7 +6,8 @@ import { Colors } from '../../../styles/Colors';
 import { FontSize } from '../../../styles/FontSize';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { commonStyles } from '../../../styles/CommonStyles';
-
+import { formatTimeToMin } from '../../../utils/TimeUtils';
+import Video from 'react-native-video';
 
 class WorkItem extends React.Component {
 
@@ -17,6 +18,7 @@ class WorkItem extends React.Component {
 
     render() {
         const data = this.props.data
+        let firTime = formatTimeToMin(data.item.data.content.data.duration);
 
         return (
             <View style={styles.root}>
@@ -42,17 +44,29 @@ class WorkItem extends React.Component {
 
                 </View>
 
-                {/* video */}
+                {this._renderVideo(data.item.data.content.data.playUrl)}
 
-                <View style={commonStyles.rowRoot}>
-                    <Ionicons style={styles.subscribeImg} name='ios-thumbs-up-outline' size={18} color={Colors.text_grey} />
-                    <Text></Text>
-                    <Ionicons style={styles.subscribeImg} name='ios-thumbs-up-outline' size={18} color={Colors.text_grey} />
-                    <Text></Text>
-                    <Text></Text>
-                    <Ionicons style={styles.subscribeImg} name='ios-thumbs-up-outline' size={18} color={Colors.text_grey} />
+                <View style={[commonStyles.rowRoot, { marginBottom: 7 }]}>
+                    <Ionicons style={styles.likeImg} name='md-heart-outline' size={18} color={Colors.text_grey} />
+                    <Text style={styles.likeCnt}>{data.item.data.content.data.consumption.collectionCount}</Text>
+                    <Ionicons style={styles.replyCnt} name='md-text' size={18} color={Colors.text_grey} />
+                    <Text style={styles.likeCnt}>{data.item.data.content.data.consumption.replyCount}</Text>
+                    <Text style={styles.duration}>{firTime}</Text>
+                    <Ionicons style={styles.shareImg} name='md-cloud-upload' size={18} color={Colors.text_grey} />
                 </View>
+
+                <View style={styles.divide} />
             </View>
+        )
+    }
+
+    _renderVideo = (videoUri) => {
+        return (
+            <Video
+                source={{ uri: videoUri }}
+                style={styles.video}
+                paused={true}
+            />
         )
     }
 }
@@ -60,7 +74,7 @@ class WorkItem extends React.Component {
 const styles = StyleSheet.create({
     root: {
         backgroundColor: 'white',
-        paddingTop: 4,
+        paddingTop: 15,
     },
     img: {
         height: 36,
@@ -77,7 +91,7 @@ const styles = StyleSheet.create({
         marginTop: 4,
         marginBottom: 7,
     },
-    tagRoot:{
+    tagRoot: {
         marginLeft: 12,
     },
     tag: {
@@ -86,6 +100,33 @@ const styles = StyleSheet.create({
         padding: 3,
         marginRight: 4,
         borderRadius: 3,
+    },
+    likeImg: {
+        marginLeft: 16,
+    },
+    likeCnt: {
+        marginLeft: 7,
+    },
+    duration: {
+        flex: 1,
+        textAlign: 'center'
+    },
+    shareImg: {
+        marginRight: 16,
+    },
+    replyCnt: {
+        marginLeft: 50,
+    },
+    video: {
+        height: 186,
+        margin: 12,
+        borderRadius: 4,
+    },
+    divide: {
+        height: 0.5,
+        backgroundColor: Colors.divide,
+        marginLeft: 12,
+        marginRight: 12,
     }
 
 });
